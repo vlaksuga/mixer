72f1f8349298b93779d352950035fc550f775cdb
vlaksuga <45654321+vlaksuga@users.noreply.github.com>
2021-09-06 15:34:38 +0900

    Classify playbuttons

--- mixer.js
+++ mixer.js
@@ -1,6 +1,8 @@
-const CLICK_COLOR = 'lightcoral'
-const DEFAULT_COLOR = 'gray'
-const BUTTON_EFFECT_DURATION = 50
+
+const BUTTON_EFFECT_DURATION = 100
+
+const playButtons = []
+const shortKey = ["q","w","e","r","a","s","d","f"]
 
 let AudioResource = [
     // initial
@@ -14,47 +16,121 @@ let AudioResource = [
     'assets/realdrums/Kick/RD_K_8.wav'
 ]
 
-// PLAY INSTRUMENT
-const insButtons = document.querySelectorAll('.player')
-insButtons.forEach((btn, idx) => {
-    btn.addEventListener('click', () => {
-        const source = new Audio(AudioResource[idx])
-        setInstButtonView(btn, source)
-        const idxColor = document.querySelectorAll('.loop')[idx].style.backgroundColor
-        if(idxColor == CLICK_COLOR) {
-            source.loop = true
+
+// MODULE : PLAY BUTTON
+class PlayButton {
+
+    CLICK_COLOR = 'turquoise'
+    DEFAULT_COLOR = 'gray'
+    CONTI = true
+
+    constructor(element, audioUrl, key) {
+        this.element = element
+        this.source = new Audio(audioUrl)
+        this.key = key
+        this.setListener()
+        this.setPlayListener()
+        this.setKey()
+    }
+
+    setListener() {
+        this.source.addEventListener('play', this.setClickStyle.bind(this))
+        return this
+    }
+
+    setClickStyle() {
+        this.element.style.background = this.CLICK_COLOR
+        setTimeout(this.setDefaulStyle.bind(this), BUTTON_EFFECT_DURATION)
+        return this
+    }
+
+    setPlayListener() {
+        this.element.addEventListener('click', this.play.bind(this))
+        return this
+    }
+
+    play() {
+        if(this.source.duration > 0 && !this.source.paused) {
+            if(this.CONTI) {
+                this.source.pause()
+                this.source.currentTime = 0
+                this.source.play()
+            }
         } else {
-            source.loop = false
-        }
-        source.play()
-    })
-})
+            this.source.play()
+        }    
+    }
 
-// SET INSTRUMENT BUTTON VIEW
-function setInstButtonView(btn, source) {
-    source.addEventListener('play', () => {
-        const style = btn.style
-        style.background = CLICK_COLOR
+    setKey() {
+        document.addEventListener('keydown', e => {
+            if(e.key === this.key) {
+                this.play()
+            }
+        })
+    }
 
-    setTimeout(() => {
-        style.background = DEFAULT_COLOR
-    }, BUTTON_EFFECT_DURATION)
+    setDefaulStyle() {
+        this.element.style.background = this.DEFAULT_COLOR
+        return this
+    }
+
+    getSource() {
+        return this.source.src
+    }
+
+    setSource(src) {
+        this.source = new Audio(src)
+        this.setListener()
+    }
+
+    getElement() {
+        return this.element
+    }
+
+    getAudio() {
+        return this.source
+    }
+}
+
+init()
+
+// INIT
+function init() {
+    console.log('init')
+    setAllPlayButtons()
+}
+
+// SET ALL PLAY BUTTONS
+function setAllPlayButtons(){
+    const insButtons = document.querySelectorAll('.player')
+    insButtons.forEach((btn, idx) => {
+        playButtons.push(new PlayButton(btn, AudioResource[idx], shortKey[idx]))
     })
 }
 
-
-// LOOP CONTROLLER
-const loopButtons = document.querySelectorAll('.loop')
-loopButtons.forEach(btn => {
+// OPEN AUDIO FILE
+const opens = document.querySelectorAll('.openFile')
+opens.forEach( (btn, idx) => {
     btn.addEventListener('click', e => {
-        console.log(e.target.style.backgroundColor)
-        if(e.target.style.backgroundColor == ""){
-            e.target.style.backgroundColor = DEFAULT_COLOR
-        }
-        if(e.target.style.backgroundColor == DEFAULT_COLOR) {
-            e.target.style.backgroundColor = CLICK_COLOR
-        } else {
-            e.target.style.backgroundColor = DEFAULT_COLOR
-        }
+        const input = document.createElement('input')
+        input.type = 'file'
+        input.click()
+        input.addEventListener('change', function(e){ readAudioResorceFile(e, idx) } , false)
     })
 })
+
+// LOAD FILE
+function readAudioResorceFile(e, idx) {
+    const file = e.target.files[0]
+    console.log(idx)
+    if(!file) { return }
+    // TODO : FILE TYPE VALIDATION HERE
+
+    const reader = new FileReader()
+    reader.onload = e => {
+        const url = URL.createObjectURL(file)
+        AudioResource[idx] = url
+        playButtons[idx].setSource(AudioResource[idx])
+    }
+    reader.readAsBinaryString(file)
+}
